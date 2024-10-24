895cafa82c26c912437bb96c0d4b0f69a6441875
Alex <alexsantana@Mac-mini-de-Alex.local>
2024-10-24 16:37:50 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import axios from "axios";
+import { load } from "cheerio"; // Utilizando named imports para cheerio
+import cors from "cors"; // Importa cors
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Usa cors middleware
+app.use(cors());
+
+app.get("/scrape-eurodreams", async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      "https://sorteoseurodreams.com/resultados/"
+    );
+    const $ = load(data);
+    const resultados = [];
+
+    // Adaptación de los selectores para extraer la fecha y los números ganadores
+    $(".lastResults__card").each((i, el) => {
+      const date = $(el)
+        .find(".lastResults__textBlock__text--date")
+        .text()
+        .trim();
+
+      const numbers = $(el)
+        .find(".lastResults__textBlock__text--result ul:first-of-type li")
+        .map((i, el) => $(el).text().trim())
+        .get();
+
+      const extraNumber = $(el).find(".lastNumbers.numdrms li").text().trim();
+
+      resultados.push({ date, numbers, extraNumber });
+    });
+
+    res.json(resultados);
+  } catch (error) {
+    console.error("Error al obtener los datos: ", error);
+    res.status(500).send("Error al obtener los datos");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor de scraping corriendo en http://localhost:${PORT}`);
+});
